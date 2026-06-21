1ac2f54011269b45da3dff07ef47dbc78f0e1ffc
Shiva04-Dev <ganeshshiva772@gmail.com>
2026-06-22 01:53:55 +0200

    Whatsapp version change

--- whatsapp.js
+++ whatsapp.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const axios = require("axios");
 
 async function sendWhatsAppMessage(to, text) {
-  const url = `https://graph.facebook.com/v20.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`;
+  const url = `https://graph.facebook.com/v19.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`;
   console.log("FINAL HEADER:", `Bearer ${process.env.WHATSAPP_TOKEN.trim()}`.slice(0, 15));
   await axios.post(
     url,
